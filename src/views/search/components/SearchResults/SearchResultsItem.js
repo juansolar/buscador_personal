7404cd48680772d601088abb24b69681f9a5b818
juansolar <juansolarteo98@gmail.com>
2023-10-05 14:22:36 -0500

    Cambio de colores y estilo para los botones

--- src/views/search/components/SearchResults/SearchResultsItem.js
+++ src/views/search/components/SearchResults/SearchResultsItem.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
-const SearchResultsItem = ({name, email}) => {
+import "./SearchResultsItem.css"
+
+const SearchResultsItem = ({name, username, email,phone,website, address}) => {
   return (
     <div className='searchResults-show' > 
+      <div className='searchResults-items'>
+        <p>Name:</p>
+        <p>Username:</p>
+        <p>Email:</p>
+        <p>Phone:</p>
+        <p>Website:</p>
+        <p>City:</p>
+      </div>
+      <div className='searchResults-results'>
         <p>{name}</p>
+        <p>{username}</p>
         <p>{email}</p>
+        <p>{phone}</p>
+        <p>{website}</p>
+        <p>{address.city} -{address.street}</p>
+      </div>
     </div>
   )
 }
